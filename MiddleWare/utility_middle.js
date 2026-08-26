f9c0e05cf5e2ee6f089dfa86ab82f20d97d4876f
Govind Singh Chouhan <govindschouhan27@gmail.com>
2026-08-26 23:52:53 +0530

    Update utility_middle.js

--- MiddleWare/utility_middle.js
+++ MiddleWare/utility_middle.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.use((req, res, next) => {
-   let {token} = req.query;
+   let {token} = req.query;    
    if(token === "giveaccess"){
      next();
    }
